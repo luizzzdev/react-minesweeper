9e20c411dc85d1d63c892aa7abdc5afc5dfe035d
Luiz Fernando Rodrigues <luiiz_roodrigues@hotmail.com>
2020-03-10 07:34:54 -0300

    initial implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useBoard, isBomb } from './minesweeper';
+
+function Line({ children }) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Cell({ children, cell, onClick, onMarkAsBomb }) {
+  return (
+    <div
+      style={{
+        width: '60px',
+        height: '60px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '1px solid #ccc',
+        margin: '5px',
+        cursor: 'pointer',
+        backgroundColor: cell.isOpen ? 'skyblue' : '',
+      }}
+      onClick={() => onClick(cell)}
+      onContextMenu={e => {
+        e.preventDefault();
+        onMarkAsBomb(cell);
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+const getCellState = cell => {
+  if (cell.markedAsBomb) return 'Bomb';
+
+  if (!cell.isOpen) return '';
+
+  if (cell.isOpen) return cell.bombsAround;
+};
 
 function App() {
+  const { board, bombs, setBoardState } = useBoard(10);
+  const [didLoose, setDidLoose] = useState(false);
+
+  const selectCell = cell => {
+    const cellIsBomb = isBomb(cell.line, cell.column, bombs);
+
+    if (cellIsBomb) {
+      setDidLoose(true);
+      return;
+    }
+
+    if (cell.markedAsBomb) return;
+
+    setBoardState(previousBoard => {
+      return previousBoard.map(line => {
+        return line.map(previousBoardCell => {
+          const isTheCellSelected =
+            previousBoardCell.line === cell.line &&
+            previousBoardCell.column === cell.column;
+          return {
+            ...previousBoardCell,
+            isOpen: isTheCellSelected || previousBoardCell.isOpen,
+          };
+        });
+      });
+    });
+  };
+
+  const markAsBomb = cell => {
+    setBoardState(previousBoard => {
+      return previousBoard.map(line => {
+        return line.map(previousBoardCell => {
+          const isTheCellSelected =
+            previousBoardCell.line === cell.line &&
+            previousBoardCell.column === cell.column;
+
+          const markedAsBomb = isTheCellSelected
+            ? !previousBoardCell.markedAsBomb
+            : previousBoardCell.markedAsBomb;
+          return {
+            ...previousBoardCell,
+            markedAsBomb,
+          };
+        });
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{didLoose ? 'You loose' : 'Keep trying'}</h1>
+
+      <div style={{ display: 'inline-block' }}>
+        {!didLoose &&
+          board.map(line => {
+            return (
+              <Line>
+                {line.map(cell => {
+                  return (
+                    <Cell
+                      cell={cell}
+                      onClick={selectCell}
+                      onMarkAsBomb={markAsBomb}
+                    >
+                      {getCellState(cell)}
+                    </Cell>
+                  );
+                })}
+              </Line>
+            );
+          })}
+      </div>
     </div>
   );
 }
---
+++ src/minesweeper.js
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+
+const generateXAndY = size => [
+  Math.floor(Math.random() * size),
+  Math.floor(Math.random() * size),
+];
+
+const isInvalidXAndY = (coordinates, x, y) => {
+  return coordinates.some(
+    coordinate => coordinate[0] === x && coordinate[1] === y
+  );
+};
+
+const generateBombs = (size, numberOfBombs) => {
+  const bombs = [];
+
+  for (let i = 0; i < numberOfBombs; i++) {
+    let [x, y] = generateXAndY(size);
+
+    while (isInvalidXAndY(bombs, x, y)) {
+      const newResult = generateXAndY(size);
+      x = newResult[0];
+      y = newResult[1];
+    }
+
+    bombs.push([x, y]);
+  }
+
+  return bombs;
+};
+
+const isBomb = (line, column, bombs) =>
+  bombs.some(bomb => bomb[0] === line && bomb[1] === column);
+
+const generateAroundFields = (line, column) => {
+  return [
+    [line - 1, column + 1],
+    [line, column + 1],
+    [line + 1, column + 1],
+    [line - 1, column],
+    [line + 1, column],
+    [line - 1, column - 1],
+    [line, column - 1],
+    [line + 1, column - 1],
+  ];
+};
+
+const generateBombsAround = (line, column, bombs) => {
+  const aroundFields = generateAroundFields(line, column);
+  let count = 0;
+
+  aroundFields.forEach(aroundField => {
+    if (isBomb(aroundField[0], aroundField[1], bombs)) {
+      count += 1;
+    }
+  });
+
+  return count;
+};
+
+const generateCell = (line, column, bombs) => {
+  return {
+    line,
+    column,
+    isBomb: isBomb(line, column, bombs),
+    open: false,
+    bombsAround: generateBombsAround(line, column, bombs),
+    markedAsBomb: false,
+  };
+};
+
+const useBoard = (size = 5) => {
+  const [bombs] = useState(generateBombs(size, size * size * 0.3));
+
+  const board = [];
+
+  for (let x = 0; x < size; x++) {
+    const line = [];
+    for (let y = 0; y < size; y++) {
+      const cell = generateCell(x, y, bombs);
+      line.push(cell);
+    }
+    board.push(line);
+  }
+
+  const [boardState, setBoardState] = useState(board);
+
+  return {
+    board: boardState,
+    setBoardState,
+    bombs,
+  };
+};
+
+const printBoard = board => {
+  board.forEach(line => {
+    let lineStr = '';
+    line.forEach(column => {
+      lineStr += column.isBomb ? ' X ' : ' - ';
+    });
+    console.log(lineStr);
+  });
+};
+
+export { useBoard, isBomb };
